1a6d1f0834db2ffb6fc9ce518d1acd9c5b08f357
MarinAK <marinkakarov@yahoo.com>
2020-04-13 15:33:48 +0300

    Update

---
+++ interne-provider/interne-provider/src/main/java/eu/itdifferentcources/internetprovider/InterneProviderApplication.java
@@ -0,0 +1,13 @@
+package eu.itdifferentcources.internetprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterneProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterneProviderApplication.class, args);
+	}
+
+}
