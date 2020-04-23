852b6d09aa13a0b60a37b65dea79a8a26876ace8
MarinAK <marinkakarov@yahoo.com>
2020-04-23 16:31:45 +0300

    Update

---
+++ internet-provider/src/main/java/eu/itdifferentcources/internetprovider/persistence/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package eu.itdifferentcources.internetprovider.persistence.repository;
+
+import eu.itdifferentcources.internetprovider.persistence.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(Role.RoleType name);
+}
---
+++ internet-provider/src/main/java/eu/itdifferentcources/internetprovider/service/UserService.java
@@ -0,0 +1,27 @@
+package eu.itdifferentcources.internetprovider.service;
+
+import eu.itdifferentcources.internetprovider.persistence.repository.UserRepository;
+import eu.itdifferentcources.internetprovider.service.exception.UserNameNotFound;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() ->
+                new UserNameNotFound(String.format("User with username %s not found", username)));
+    }
+}
