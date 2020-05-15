8d374d429a5ff16a781b1e99fb8e6df544649973
MarinAK <marinkakarov@yahoo.com>
2020-05-15 09:47:00 +0300

    save work

--- internet-provider/src/main/java/eu/itdc/internetprovider/service/dto/UserUpdateRolesDTO.java
+++ internet-provider/src/main/java/eu/itdc/internetprovider/service/dto/UserUpdateRolesDTO.java
@@ -1,23 +1,37 @@
 package eu.itdc.internetprovider.service.dto;
 
+import eu.itdc.internetprovider.persistence.entity.Role;
+
 import java.util.List;
 
 public class UserUpdateRolesDTO {
 
-    private List<String> roles;
-
-    public UserUpdateRolesDTO(List<String> roles) {
-        this.roles = roles;
-    }
+//    private List<String> roles;
+//
+//    public UserUpdateRolesDTO(List<String> roles) {
+//        this.roles = roles;
+//    }
+//
+//    public UserUpdateRolesDTO() {
+//    }
+//
+//    public List<String> getRoles() {
+//        return roles;
+//    }
+//
+//    public void setRoles(List<String> roles) {
+//        this.roles = roles;
+//    }
+    private List<RoleTypeEnum> roles;
 
     public UserUpdateRolesDTO() {
     }
 
-    public List<String> getRoles() {
+    public List<RoleTypeEnum> getRoles() {
         return roles;
     }
 
-    public void setRoles(List<String> roles) {
+    public void setRoles(List<RoleTypeEnum> roles) {
         this.roles = roles;
     }
 }
