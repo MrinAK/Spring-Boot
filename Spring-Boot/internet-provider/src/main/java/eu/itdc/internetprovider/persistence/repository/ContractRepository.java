73baec0a8f90c59ff67afbdaaf5b011126bf7403
Marin Kakarov <60268660+MrinAK@users.noreply.github.com>
2020-06-10 17:53:22 +0300

    Delete ContractRepository.java

--- Spring-Boot/internet-provider/src/main/java/eu/itdc/internetprovider/persistence/repository/ContractRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.itdc.internetprovider.persistence.repository;
-
-import eu.itdc.internetprovider.persistence.entity.Contract;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContractRepository extends JpaRepository<Contract, Long> {
-}
